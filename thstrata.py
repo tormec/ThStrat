0f065775fb7e4cfa2671cd442af35adf4e248a64
tormec <dcstoyano@gmail.com>
2020-03-19 23:35:26 +0100

    added latex class

--- thstrata.py
+++ thstrata.py
@@ -7,23 +7,23 @@ class Transmittance(object):
     * double-slash separetd values: material in parallel
     * comma separated values in brackets: material in series within parallel
     For instance:
-    pattern = '1,(2,3,4)//5//(6,7),8'
+    pattern = "1,(2,3,4)//5//(6,7),8"
 
     The indexes used in the pattern are different from the ones used to
     identify different materials.
     For instance:
-    pattern = '1,2,3'
+    pattern = "1,2,3"
     stratigraphy = {
-        '1': {'mat': 1, 'thk': 1, 'area': 1, 'cnd': .01},
-        '2': {'mat': 2, 'thk': 1, 'area': 1, 'rst': .02},
-        '3': {'mat': 1, 'thk': 1, 'area': 1, 'cnd': .01}
+        "1": {"mat": 1, "thk": 1, "area": 1, "cnd": .01},
+        "2": {"mat": 2, "thk": 1, "area": 1, "rst": .02},
+        "3": {"mat": 1, "thk": 1, "area": 1, "cnd": .01}
         }
     where:
-    'mat': material
-    'thk': thickness
-    'area'
-    'cnd': conduttivity
-    'rst': resistance
+    "mat": material
+    "thk": thickness
+    "area"
+    "cnd": conduttivity
+    "rst": resistance
     """
     def __init__(self, pattern, strat, area):
         self.transmittance = 1 / self.global_resistance(pattern, strat, area)
@@ -37,18 +37,18 @@ class Transmittance(object):
         :param area (float): surface of the stratigraphy
         :return resistante (flot): the resistance [(m^2 K)/W]
         """
-        pattern = pattern.replace(' ', '')
+        pattern = pattern.replace(" ", "")
         pattern = self.split_series(pattern)
 
         # calc the resistance of the structure
         rst = []
         for i in pattern:
-            i = i.split('//')  # indexes in parallel (if any)
+            i = i.split("//")  # indexes in parallel (if any)
             if len(i) > 1:
                 p_rst = []
                 for p in i:
-                    p = p.strip('()')
-                    p = p.split(',')  # indexes in series in parallel (if any)
+                    p = p.strip("()")
+                    p = p.split(",")  # indexes in series in parallel (if any)
                     if len(p) > 1:
                         s_rst = []
                         for s in p:
@@ -66,24 +66,24 @@ class Transmittance(object):
         """Split the pattern into indexes in series.
 
         For instance:
-        '1,(2,3,4)//5//(6,7),8'
+        "1,(2,3,4)//5//(6,7),8"
         is splitted in:
-        ['1', '(2,3,4)//5//(6,7)', '8']
+        ["1", "(2,3,4)//5//(6,7)", "8"]
 
         :param pattern (str): description of how materials are set out
         :return series (list): indexes or chunks of the pattern in series
         """
         series = []
-        idx = ''
+        idx = ""
         inparallel = 0
         for i, v in enumerate(pattern):
-            if v == ',' and inparallel == 0:
+            if v == "," and inparallel == 0:
                 series.append(idx)
-                idx = ''
-            elif v == '(':
+                idx = ""
+            elif v == "(":
                 inparallel = 1
                 idx = idx + str(v)
-            elif v == ')' and pattern[i+1] == ',':
+            elif v == ")" and pattern[i+1] == ",":
                 inparallel = 0
                 idx = idx + str(v)
             elif i == len(pattern) - 1:
@@ -101,30 +101,71 @@ class Transmittance(object):
         :return rst (float): the resistance
         """
         rst = 0
-        if 'cnd' in strat[idx]:
-            rst = strat[idx]['thk'] / strat[idx]['cnd']  # (m^2 K)/W
+        if "cnd" in strat[idx]:
+            rst = strat[idx]["thk"] / strat[idx]["cnd"]  # (m^2 K)/W
         else:
-            rst = strat[idx]['rst']  # (m^2 K)/W
-        rst = rst / strat[idx]['area']  # K/W
+            rst = strat[idx]["rst"]  # (m^2 K)/W
+        rst = rst / strat[idx]["area"]  # K/W
         return rst
 
 
-def main(self):
-    pattern = '1, (2,3,4)//5//(6,7), 8'
+class Latex(Transmittance):
+    def __init__(self, pattern, stratigraphy, area, filename, lang):
+        super().__init__(pattern, stratigraphy, area)
+
+        preamble = self.preamble(lang)
+        table = self.table()
+        with open(filename, "w") as f:
+            f.write("\n".join(preamble))
+            f.write("\n\\begin{document}\n")
+            f.write("\n".join(table))
+            f.write("\n\\end{document}")
+            f.closed
+
+    def preamble(self, lang):
+        """Preamble.
+
+        :param lang (str): language to use for babel package
+        return preamble (list): the preamble
+        """
+        preamble = ["\\documentclass[10pt,a4paper]{article}",
+                    "\\usepackage[utf8]{inputenc}",
+                    "\\usepackage[{}]{{babel}}".format(lang),
+                    "\\usepackage{amsmath}",
+                    "\\usepackage{amsfonts}",
+                    "\\usepackage{amssymb}",
+                    "\\usepackage{graphicx}",
+                    "\\usepackage[left=2cm,right=2cm,"
+                    "top=2cm,bottom=2cm]{geometry}",
+                    "\\usepackage{caption}",
+                    "\\usepackage{siunitx}"]
+        return preamble
+
+    def table(self):
+        """Table result of the stratigraphy.
+        return table (list): the table
+        """
+        pass
+
+
+def main():
+    pattern = "1, (2,3,4)//5//(6,7), 8"
     stratigraphy = {
-        '1': {'mat': 1, 'thk': 1, 'area': 3, 'cnd': .1},
-        '2': {'mat': 2, 'thk': 1, 'area': 1, 'rst': .2},
-        '3': {'mat': 3, 'thk': 1, 'area': 1, 'cnd': .3},
-        '4': {'mat': 2, 'thk': 1, 'area': 1, 'rst': .4},
-        '5': {'mat': 4, 'thk': 3, 'area': 1, 'cnd': .5},
-        '6': {'mat': 3, 'thk': 1.5, 'area': 1, 'cnd': .6},
-        '7': {'mat': 2, 'thk': 1.5, 'area': 1, 'rst': .7},
-        '8': {'mat': 1, 'thk': 1, 'area': 3, 'cnd': .8}
+        "1": {"mat": 1, "thk": 1, "area": 3, "cnd": .1},
+        "2": {"mat": 2, "thk": 1, "area": 1, "rst": .2},
+        "3": {"mat": 3, "thk": 1, "area": 1, "cnd": .3},
+        "4": {"mat": 2, "thk": 1, "area": 1, "rst": .4},
+        "5": {"mat": 4, "thk": 3, "area": 1, "cnd": .5},
+        "6": {"mat": 3, "thk": 1.5, "area": 1, "cnd": .6},
+        "7": {"mat": 2, "thk": 1.5, "area": 1, "rst": .7},
+        "8": {"mat": 1, "thk": 1, "area": 3, "cnd": .8}
     }
     area = 3
+    filename = "testStrata.tex"
+    lang = "english"
 
-    Transmittance(pattern, stratigraphy, area)
+    Latex(pattern, stratigraphy, area, filename, lang)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
